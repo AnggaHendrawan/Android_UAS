dbca1098cb2d007a321396021699656c49ba2f35
AnggaHendrawan <angga100495@gmail.com>
2018-08-22 16:36:50 +0700

    v-01

---
+++ app/src/main/java/com/unit_01/angga/Server.java
@@ -0,0 +1,10 @@
+package com.unit_01.angga;
+
+/**
+ * Created by Angga Hendrawan on 13/08/2018.
+ */
+
+public class Server {
+    public static final String URL =
+            "http://sasmitoh.nitarahmawati.my.id/";
+}
